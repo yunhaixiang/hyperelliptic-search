c15ee3ff5b09a34adfaea0db65dd38fbebb800ff
Yunhai Xiang <10103526+yunhaixiang@users.noreply.github.com>
2026-06-02 21:52:44 -0400

    edit

--- src/envs/__init__.py
+++ src/envs/__init__.py
@@ -1,20 +1,42 @@
-from src.envs.cycle import SquareEnvironment
-from src.envs.hyperelliptic import HyperellipticEnvironment
-from src.envs.hyperelliptic_highgenus import HighGenusHyperellipticEnvironment
-from src.envs.hyperelliptic_multigenus import MultiGenusHyperellipticEnvironment
-from src.envs.hyperelliptic2 import Hyperelliptic2Environment
-from src.envs.isosceles import IsoscelesEnvironment
-from src.envs.sphere import SphereEnvironment
+from importlib import import_module
 
-ENVS = {
-    "square": SquareEnvironment,
-    "isosceles": IsoscelesEnvironment,
-    "sphere": SphereEnvironment,
-    "hyperelliptic": HyperellipticEnvironment,
-    "hyperelliptic_highgenus": HighGenusHyperellipticEnvironment,
-    "hyperelliptic_multigenus": MultiGenusHyperellipticEnvironment,
-    "hyperelliptic2": Hyperelliptic2Environment,
+
+_ENV_SPECS = {
+    "square": ("src.envs.cycle", "SquareEnvironment"),
+    "isosceles": ("src.envs.isosceles", "IsoscelesEnvironment"),
+    "sphere": ("src.envs.sphere", "SphereEnvironment"),
+    "hyperelliptic": ("src.envs.hyperelliptic", "HyperellipticEnvironment"),
+    "hyperelliptic_highgenus": ("src.envs.hyperelliptic_highgenus", "HighGenusHyperellipticEnvironment"),
+    "hyperelliptic_multigenus": ("src.envs.hyperelliptic_multigenus", "MultiGenusHyperellipticEnvironment"),
+    "hyperelliptic2": ("src.envs.hyperelliptic2", "Hyperelliptic2Environment"),
 }
+
+
+class LazyEnvironmentRegistry:
+    def __init__(self, specs):
+        self.specs = dict(specs)
+        self.cache = {}
+
+    def __contains__(self, key):
+        return key in self.specs
+
+    def __getitem__(self, key):
+        if key not in self.specs:
+            raise KeyError(key)
+        if key not in self.cache:
+            module_name, class_name = self.specs[key]
+            self.cache[key] = getattr(import_module(module_name), class_name)
+        return self.cache[key]
+
+    def keys(self):
+        return self.specs.keys()
+
+    def items(self):
+        for key in self.specs:
+            yield key, self[key]
+
+
+ENVS = LazyEnvironmentRegistry(_ENV_SPECS)
 
 
 def build_env(params):
